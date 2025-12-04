e7f516ac1e647ad5da2d7479b67274f53f3f9b7d
AntonButov <58133570+AntonButov@users.noreply.github.com>
2025-12-04 14:36:48 +0300

    Simplify getBaseUrl for JS platform - always use https://hostname/api/
    - Remove domain-specific checks
    - Always use current hostname with HTTPS
    - Works for any domain (dev.drivebit.my, drivebit.my, localhost, etc.)

--- Network/src/jsMain/kotlin/my/drivebit/network/HttpClientFactory.js.kt
+++ Network/src/jsMain/kotlin/my/drivebit/network/HttpClientFactory.js.kt
@@ -7,14 +7,6 @@ import kotlinx.browser.window
 actual fun createPlatformHttpClientEngine(): HttpClientEngine = Js.create()
 
 actual fun getBaseUrl(): String {
-    val protocol = window.location.protocol
     val hostname = window.location.hostname
-
-    // Для dev.drivebit.my и drivebit.my используем прокси через тот же домен
-    // nginx проксирует /api/ -> http://api.drivebit.my:5000/
-    return if (hostname == "dev.drivebit.my" || hostname == "drivebit.my") {
-        "$protocol//$hostname/api/"
-    } else {
-        DEFAULT_BASE_URL
-    }
+    return "https://$hostname/api/"
 }
