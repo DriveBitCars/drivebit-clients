c6d160102d4d100c5f81e7ceffd1095c912b2c15
AntonButov <58133570+AntonButov@users.noreply.github.com>
2025-12-04 14:39:41 +0300

    Use DEFAULT_BASE_URL for all platforms (JS, Android, iOS)
    - All platforms now use https://drivebit.my/api/
    - Removed dynamic hostname logic from JS platform
    - Consistent base URL across all platforms

--- Network/src/jsMain/kotlin/my/drivebit/network/HttpClientFactory.js.kt
+++ Network/src/jsMain/kotlin/my/drivebit/network/HttpClientFactory.js.kt
@@ -2,11 +2,7 @@ package my.drivebit.network
 
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.js.Js
-import kotlinx.browser.window
 
 actual fun createPlatformHttpClientEngine(): HttpClientEngine = Js.create()
 
-actual fun getBaseUrl(): String {
-    val hostname = window.location.hostname
-    return "https://$hostname/api/"
-}
+actual fun getBaseUrl(): String = DEFAULT_BASE_URL
