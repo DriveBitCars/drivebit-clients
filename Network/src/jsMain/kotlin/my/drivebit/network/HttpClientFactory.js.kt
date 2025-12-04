5392a45dcb738b9e8128a3ac8d2b3b63e3dbf05a
AntonButov <58133570+AntonButov@users.noreply.github.com>
2025-12-04 14:44:57 +0300

    Remove getBaseUrl() expect/actual pattern, use DEFAULT_BASE_URL directly
    - All platforms now use DEFAULT_BASE_URL constant directly
    - Removed unnecessary expect/actual pattern for getBaseUrl()
    - Simplified code by removing platform-specific implementations
    - All services now use DEFAULT_BASE_URL directly

--- Network/src/jsMain/kotlin/my/drivebit/network/HttpClientFactory.js.kt
+++ Network/src/jsMain/kotlin/my/drivebit/network/HttpClientFactory.js.kt
@@ -4,5 +4,3 @@ import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.js.Js
 
 actual fun createPlatformHttpClientEngine(): HttpClientEngine = Js.create()
-
-actual fun getBaseUrl(): String = DEFAULT_BASE_URL
